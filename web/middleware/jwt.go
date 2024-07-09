e03932a994b055c38b004d5c5b4017844c5a355b
fengqi <lyf362345@gmail.com>
2024-07-09 06:21:28 +0000

    jwt key调整

--- web/middleware/jwt.go
+++ web/middleware/jwt.go
@@ -9,35 +9,32 @@ import (
 )
 
 // JwtAuth Jwt验证
-// todo 可能不需要上jwt这种验证方式
 func JwtAuth(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		// get jwt token from query string or header or cookie
-		jwtToken := c.QueryParam(setting.JwtKey)
+		jwtToken := c.Request().Header.Get("Authorization")
 		if jwtToken == "" {
-			jwtToken = c.Request().Header.Get(setting.JwtKey)
-			if jwtToken == "" {
-				cookie, err := c.Cookie(setting.JwtKey)
-				if err == nil && cookie != nil {
-					jwtToken = cookie.Value
-				}
+			cookie, err := c.Cookie(setting.JwtKey)
+			if err == nil && cookie != nil {
+				jwtToken = cookie.Value
 			}
 		}
-		if jwtToken == "" {
+
+		if jwtToken == "" || len(jwtToken) < 8 || jwtToken[0:6] != "Bearer" {
 			return utils.ApiError(c, model.ErrorLoginFailed)
 		}
 
+		jwtToken = jwtToken[7:]
 		token, err := jwt.Parse(jwtToken, func(token *jwt.Token) (interface{}, error) {
 			return setting.JwtSecret, nil
 		})
-		if err == nil || token == nil || !token.Valid {
+		if err != nil || token == nil || !token.Valid {
 			return utils.ApiError(c, model.ErrorLoginFailed)
 		}
 
 		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
 			c.Set("jwt-claims", claims)
 			if uid, ok := claims["uid"]; ok {
-				c.Set("uid", utils.Str2Int(uid.(string)))
+				c.Set("uid", uid)
 			}
 		}
 
