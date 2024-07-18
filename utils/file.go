c9bcb63c26623e255e402c5aff0368d5bf27e8fa
fengqi <lyf362345@gmail.com>
2024-07-18 19:14:49 +0800

    判断目录、文件是否存在

--- utils/file.go
+++ utils/file.go
@@ -1,11 +1,33 @@
 package utils
 
-import "os"
+import (
+	"os"
+)
 
-func FileExists(path string) bool {
-	if fi, err := os.Stat(path); fi != nil && err != os.ErrNotExist {
-		return !fi.IsDir()
+// FileExist 判断文件是否存在
+func FileExist(path string) bool {
+	if path == "" {
+		return false
 	}
 
-	return false
+	fi, err := os.Stat(path)
+	if err != nil {
+		return !os.IsNotExist(err)
+	}
+
+	return !fi.IsDir()
+}
+
+// PathExist 判断目录是否存在
+func PathExist(path string) bool {
+	if path == "" {
+		return false
+	}
+
+	fi, err := os.Stat(path)
+	if err != nil {
+		return !os.IsNotExist(err)
+	}
+
+	return fi.IsDir()
 }
