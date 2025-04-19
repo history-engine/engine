bb878acc4d0d488c9b25ae5f076854c7289b1ac9
fengqi <lyf362345@gmail.com>
2025-04-19 19:47:26 +0800

    添加ApiServerError

--- utils/response.go
+++ utils/response.go
@@ -3,10 +3,11 @@ package utils
 import (
 	"github.com/labstack/echo/v4"
 	"history-engine/engine/model"
+	"net/http"
 )
 
 func ApiResponse(ctx echo.Context, code int, msg string, data any) error {
-	return ctx.JSON(200, model.ApiResponse{
+	return ctx.JSON(http.StatusOK, model.ApiResponse{
 		Code:    0,
 		Message: msg,
 		Data:    data,
@@ -14,7 +15,7 @@ func ApiResponse(ctx echo.Context, code int, msg string, data any) error {
 }
 
 func ApiSuccess(ctx echo.Context, data any) error {
-	return ctx.JSON(200, model.ApiResponse{
+	return ctx.JSON(http.StatusOK, model.ApiResponse{
 		Code:    0,
 		Message: "ok",
 		Data:    data,
@@ -28,7 +29,21 @@ func ApiError(ctx echo.Context, code model.MsgCode) error {
 		msg = model.MsgTable[model.Unknown]
 	}
 
-	return ctx.JSON(200, model.ApiResponse{
+	return ctx.JSON(http.StatusOK, model.ApiResponse{
+		Code:    code,
+		Message: msg,
+		Data:    nil,
+	})
+}
+
+func ApiServerError(ctx echo.Context, code model.MsgCode) error {
+	var ok bool
+	msg := "ok"
+	if msg, ok = model.MsgTable[code]; !ok {
+		msg = model.MsgTable[model.Unknown]
+	}
+
+	return ctx.JSON(http.StatusInternalServerError, model.ApiResponse{
 		Code:    code,
 		Message: msg,
 		Data:    nil,
